cbf5c63cf1c66f4f836a0ec60ade640d5cd6d779
mika <kaszuba.mika@gmail.com>
2015-11-06 21:28:17 +0100

    common code initial commit

---
+++ Common/DnxInformation.cs
@@ -0,0 +1,23 @@
+﻿namespace Common
+{
+    using System.Diagnostics;
+    using System.IO;
+
+    public static class DnxInformation
+    {
+        public static string GetDnxPath()
+        {
+            return Path.GetDirectoryName(Process.GetCurrentProcess().Modules[0].FileName);
+        }
+
+        public static string GetDnx()
+        {
+            return Path.Combine(GetDnxPath(), "dnx.exe");
+        }
+
+        public static string GetDnu()
+        {
+            return Path.Combine(GetDnxPath(), "dnu.cmd");
+        }
+    }
+}
